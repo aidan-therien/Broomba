d13e7dc2531895406031d49e67d0403d65a58f13
bradhoward1 <65506495+bradhoward1@users.noreply.github.com>
2021-04-14 21:10:27 -0400

    Add nunchuk_main file

---
+++ nunchuk_main.cpp
@@ -0,0 +1,24 @@
+
+#include "mbed.h"
+#include "nunchuk.h"
+
+// I2C i2c(I2C_SDA, I2C_SCL);
+// main() runs in its own thread in the OS
+int main()
+{
+    // i2c.frequency(400000);
+    printf("starting\n\n");
+    if(!nunchuk_init()){
+        printf("Init failed\n");
+        while(1);
+    }
+    while (true) {
+        if (!nunchuk_read()){
+            printf("read failed\n");
+        }
+        else{
+            nunchuk_print();
+        }
+        wait_us(500000);
+    }
+}
